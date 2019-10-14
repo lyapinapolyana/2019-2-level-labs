078508c508398b624dda12813b5547db69a96d3d
lyapinapolyana <polyana_schumacher@mail.ru>
2019-10-14 12:07:07 +0300

    итоговая версия 2.0

--- lab_1/main.py
+++ lab_1/main.py
@@ -5,10 +5,7 @@ Count frequencies dictionary by the given arbitrary text
 
 
 text = ('''
-Hi! 
-I am James. I am 24.
-Nice to meet you, Buddy!
-While i was is in China you have become very dull. I very like to meet new people:) Do you like me? Blah Blah Blah
+
 ''')
 
 stop_words = ('ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about',
@@ -25,90 +22,64 @@ stop_words = ('ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there
               'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing',
               'it', 'how', 'further', 'was', 'here', 'than')
 
-stop_symbols = ['!','?',';',':',')','(','@','#','$','%','^','&','*','-','/','.',',','1','2','3','4','5','6','7','8','9','0']
+stop_symbols = ['!', '?', ';', ':', ')', '(', '@', '#', '$', '%', '^', '&', '*', '-', '/', '.', ',', '~', '@', '$', '%', '\n', '"', "'", '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 
 def calculate_frequences(text):
-    global frequencies
-    if len(text) == 0:
-        print ('error.')
-        frequencies = {}
+    frequencies = {}
+    if text is None or len(str(text)) == 0:
         return frequencies
     else:
-        if text is None:
-            print ('text was not given.')
-            frequencies = {}
+        if not isinstance(text, str):
             return frequencies
+    text1 = text.lower()
+    for symbol in text1:
+        if symbol in stop_symbols:
+            text1 = text1.replace(symbol, '  ')
+        text2 = text1.split()
+    for key in text2:
+        if key in frequencies:
+            value = frequencies[key]
+            frequencies[key] = value + 1
         else:
-            if not isinstance(text, str):
-                print ('error.')
-                frequencies = {}
-                return frequencies
-            else:
-                text1 = text.lower()
-                text2 = str(text1)
-                for symbol in stop_symbols:
-                    if symbol in text2:
-                        text2 = text2.replace(symbol, '')
-                        text3 = text2.split()
-                        frequencies = {}
-                        for word in text3:
-                            if word in frequencies:
-                                frequencies[word] += 1
-                            else:
-                                frequencies[word] = 1
-                return frequencies
+            frequencies[key] = 1
+    return frequencies
 
 
 def filter_stop_words(frequencies, stop_words):
-    global no_stop_words_frequencies
     if frequencies is None:
-        print ('error.')
-        return {}
+        return frequencies
     elif len(frequencies) == 0:
-        print ('error.')
-        return {}
+        return frequencies
     elif type(frequencies) is not dict:
-        print ('error.')
-        return {}
+        return frequencies
     else:
         if stop_words is None:
-            print ('error.')
-            return {}
+            return frequencies
         elif len(stop_words) == 0:
-            print('error.')
-            return {}
+            return frequencies
         else:
             no_stop_words_frequencies = frequencies.copy()
             for stop_word in stop_words:
                 if no_stop_words_frequencies.get(stop_word):
                     no_stop_words_frequencies.pop(stop_word)
             for key in no_stop_words_frequencies:
-                if key in stop_words or isinstance (key, int):
+                if key in stop_words or isinstance(key, int):
                     no_stop_words_frequencies.pop(key)
-                    return (no_stop_words_frequencies)
+                    return no_stop_words_frequencies
             return no_stop_words_frequencies
 
 
 def get_top_n(no_stop_words_frequencies, top_n):
-    if top_n == 0 or top_n < 0:
-        print ('error!')
-        return ()
+    if not isinstance(top_n, int):
+        no_stop_words_frequencies = ()
+        return no_stop_words_frequencies
     else:
-        if no_stop_words_frequencies == {}:
-            print ('error!')
-            return ()
+        if top_n < 0:
+            top_n = 0
         else:
-            list_frequencies = list(no_stop_words_frequencies.items())
-            sorted_list_frequencies = sorted(list_frequencies, key=lambda value: value[1], reverse = True)
-            print(sorted_list_frequencies)
-            final_frequencies = tuple(sorted_list_frequencies[:top_n])
-            print (final_frequencies)
-
-
-calculate_frequences(text)
-filter_stop_words(frequencies, stop_words)
-get_top_n(no_stop_words_frequencies, top_n = 6)
-
-
-
+            if top_n > len(no_stop_words_frequencies):
+                top_n = len(no_stop_words_frequencies)
+    sorted_frequencies = sorted(no_stop_words_frequencies, key=lambda x: int(no_stop_words_frequencies[x]), reverse=True)
+    final_frequencies = tuple(sorted_frequencies[:top_n])
+    return final_frequencies
